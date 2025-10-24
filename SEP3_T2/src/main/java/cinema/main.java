d9bc89380a59b46db92ec92b32a0c133c48747c5
Rasmus Duus <342727@via.dk>
2025-10-24 09:46:48 +0200

    restruktureret til maven projekt som i PRO3

--- SEP3_T2/src/main/java/cinema/main.java
+++ SEP3_T2/src/main/java/cinema/main.java
@@ -1,6 +1,8 @@
-import model.Customer;
-import persistence.CustomerDAO;
-import persistence.CustomerDAOImpl;
+package cinema;
+
+import cinema.model.Customer;
+import cinema.persistence.CustomerDAO;
+import cinema.persistence.CustomerDAOImpl;
 
 public class main
   {
@@ -9,9 +11,9 @@ public class main
               CustomerDAO dao = CustomerDAOImpl.getInstance();
 
               // Create a new customer
-              Customer newCustomer = new Customer("Rasmus Duus", "HelloMartin1717!", "342727@via.dk", "30439697");
-              dao.createCustomer(newCustomer);
-              System.out.println("Customer created.");
+//              Customer newCustomer = new Customer("Rasmus Duus", "HelloMartin1717!", "342727@via.dk", "30439697");
+//              dao.createCustomer(newCustomer);
+//              System.out.println("Customer created.");
 
               // Retrieve the customer by email
               Customer retrieved = dao.getCustomerByEmail("342727@via.dk");
