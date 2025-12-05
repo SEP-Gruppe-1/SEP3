d30b554cb46f890f6b1039a7d671f261ace51ce9
Marx <354405@via.dk>
2025-12-05 19:59:56 +0100

    MovieDetails Page + app.css

--- SEP3_T1/BlazorApp1/Program.cs
+++ SEP3_T1/BlazorApp1/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddScoped<ICustomerRepository, CustomerInDatabaseRepository>();
 // builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddSingleton(new CinemaServiceClient("http://localhost:9090"));
 builder.Services.AddScoped<IMovieRepository, MovieInRepository>();
+builder.Services.AddScoped<IScreeningRepository, ScreeningInRepository>();
 
 // FØRST HER KALDER DU builder.Build()
 var app = builder.Build();
