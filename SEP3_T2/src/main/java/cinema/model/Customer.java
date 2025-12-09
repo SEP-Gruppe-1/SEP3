ca6a0df628a4791f722fcedbec1f8e4c7c0d2f50
Rasmus Duus <342727@via.dk>
2025-12-09 11:26:51 +0100

    Hasher nu passwords ved creation og returnerer ikke passwords når vi henter customer/s

--- SEP3_T2/src/main/java/cinema/model/Customer.java
+++ SEP3_T2/src/main/java/cinema/model/Customer.java
@@ -1,59 +1,79 @@
 package cinema.model;
 
-public class Customer {
-    private String name;
-    private String password;
-    private String email;
-    private String phone;
-    private String role;
+public class Customer
+{
+  private String name;
+  private String password;
+  private String email;
+  private String phone;
+  private String role;
 
+  public Customer(String name, String password, String email, String phone,
+      String role)
+  {
+    this.name = name;
+    this.password = password;
+    this.email = email;
+    this.phone = phone;
+    this.role = role;
+  }
 
-    public Customer(String name, String password, String email, String phone, String role) {
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.phone = phone;
-        this.role = role;
-    }
+  public Customer(String name, String email, String phone, String role)
+  {
+    this.name = name;
+    this.password = "encrypted";
+    this.email = email;
+    this.phone = phone;
+    this.role = role;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName()
+  {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name)
+  {
+    this.name = name;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword()
+  {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password)
+  {
+    this.password = password;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail()
+  {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email)
+  {
+    this.email = email;
+  }
 
-    public String getPhone() {
-        return phone;
-    }
+  public String getPhone()
+  {
+    return phone;
+  }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+  public void setPhone(String phone)
+  {
+    this.phone = phone;
+  }
 
-    public String getRole() {
-        return role;
-    }
+  public String getRole()
+  {
+    return role;
+  }
 
-    public void setRole(String role)
-    {
-        this.role = role;
-    }
+  public void setRole(String role)
+  {
+    this.role = role;
+  }
 }
