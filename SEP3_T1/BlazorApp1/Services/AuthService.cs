33544263cf04ebb038acded4e981454fd8de292b
Martin Andersen <Guffe2010@gmail.com>
2025-12-15 12:26:43 +0100

    Dokumentation og smårettelser

--- SEP3_T1/BlazorApp1/Services/AuthService.cs
+++ SEP3_T1/BlazorApp1/Services/AuthService.cs
@@ -4,7 +4,10 @@ public class AuthService
 {
     private readonly HttpClient http;
     private readonly IJSRuntime js;
-
+/// <summary>
+/// Auth Service Constructor
+/// </summary>
+/// <param name="js"></param>
     public AuthService(IJSRuntime js)
     {
         this.js = js;
@@ -13,6 +16,14 @@ public class AuthService
             BaseAddress = new Uri("http://localhost:5099/")
         };
     }
+
+/// <summary>
+/// Login And Return Token
+/// </summary>
+/// <param name="phone"></param>
+/// <param name="password"></param>
+/// <returns></returns>
+/// <exception cref="Exception"></exception>
 
     public async Task<string> LoginAndReturnToken(string? phone, string password)
     {
@@ -34,12 +45,18 @@ public class AuthService
 
         return tokenResponse.Token;
     }
-
+/// <summary>
+/// Logout
+/// </summary>
   
     public async Task LogoutAsync()
     {
         await js.InvokeVoidAsync("localStorage.removeItem", "jwt");
     }
+
+/// <summary>
+/// Token Response Class
+/// </summary>
 
     private class TokenResponse
     {
